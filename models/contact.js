80649f1476d436509e3a144523805c6b64e2c8da
OlhaPetrenko <o.l.petr27@gmail.com>
2022-11-18 13:12:36 +0200

    Update contact.js

--- models/contact.js
+++ models/contact.js
@@ -40,7 +40,6 @@ const addScheme = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().pattern(phoneRegexp).required(),
-  // phone: Joi.string().required(),
   favorite: Joi.boolean(),
 });
 
